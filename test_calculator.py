9c7ea5795ff874b3e5f9e00d86398cbb5ea765d4
Bradley Gore <gorebradleyi@gmail.com>
2021-03-11 09:18:05 -0800

    Fix Multiply Test

--- test_calculator.py
+++ test_calculator.py
@@ -13,5 +13,5 @@ class TestCalculator:
     def test_subtract(self):
         assert 2 == calculator.subtract(5, 3)
 
-    def test_subtract(self):
+    def test_multiply(self):
         assert 10 == calculator.multiply(5, 2)
